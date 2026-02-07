08d155a8d7feacb9d3d8a087aea9870fb9667e7f
sinaTejeh <amrjv1378@gmail.com>
2026-02-07 22:32:59 +0330

    rendering the results in a list and calculate more value

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,9 +1,38 @@
-import { calculateInvestmentResults } from '../util/investment.js';
+import { calculateInvestmentResults, formatter } from '../util/investment.js';
 
 export default function Results({ input }) {
     const data = calculateInvestmentResults(input);
-    console.log(data);
+    const initialInvestment = data[0].valueEndOfYear - data[0].interest - data[0].annualInvestment;
     return (
-        <p></p>
+        <table id='result'>
+            <thead>
+                <tr>
+                    <th>Year</th>
+                    <th>Investment Value</th>
+                    <th>Interest (Year)</th>
+                    <th>Total Interest</th>
+                    <th>Invested Capital</th>
+                </tr>
+            </thead>
+            <tbody>
+                {data.map(yaerData => {
+                    const totalInterestValue =
+                        yaerData.valueEndOfYear -
+                        yaerData.annualInvestment *
+                        yaerData.year -
+                        initialInvestment;
+                    const totalAmountInvested = yaerData.valueEndOfYear - totalInterestValue;
+                    return (
+                        <tr key={yaerData.year}>
+                            <td>{yaerData.year}</td>
+                            <td>{formatter.format(yaerData.valueEndOfYear)}</td>
+                            <td>{formatter.format(yaerData.interest)}</td>
+                            <td>{formatter.format(totalInterestValue)}</td>
+                            <td>{formatter.format(totalAmountInvested)}</td>
+                        </tr>
+                    );
+                })}
+            </tbody>
+        </table>
     );
 };
